1842b924ea974be370640f41f4a7054b3cb35a7c
Atharva Deosthale <atharva.deosthale17@gmail.com>
2024-01-11 20:41:36 +0530

    chore: add hashnode CDN to nextjs config, todo replace with optimized image tag

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.hashnode.com",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
